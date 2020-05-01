dc68a7eb06d35834297c11e58a539c26c9210ca3
DEVfancybear <duonghacker2345@gmail.com>
2020-05-01 21:42:26 +0700

    create button change languages component App

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <nav style={{width: '100%', padding: '2rem 0', backgroundColor: 'gray'}}>
+                <button>
+                    English
+                </button>
+                <button>
+                    Korean
+                </button>
+                <button>
+                    Chinese
+                </button>
+                <button>
+                    Vietnam
+                </button>
+            </nav>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
