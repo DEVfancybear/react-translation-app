c2149e392661c6a3496a8c69b1bf6204cb34f98b
DEVfancybear <duonghacker2345@gmail.com>
2020-05-01 21:49:37 +0700

    create folder localse

--- src/App.js
+++ src/App.js
@@ -12,32 +12,22 @@ const App = () => {
     return (
         <div className="App">
             <nav style={{width: '100%', padding: '2rem 0', backgroundColor: 'gray'}}>
-                <button onClick={()=>handleChangeLanguages('en')}>
+                <button onClick={() => handleChangeLanguages('en')}>
                     English
                 </button>
-                <button onClick={()=>handleChangeLanguages('ko')}>
+                <button onClick={() => handleChangeLanguages('ko')}>
                     Korean
                 </button>
-                <button onClick={()=>handleChangeLanguages('chi')}>
+                <button onClick={() => handleChangeLanguages('chi')}>
                     Chinese
                 </button>
-                <button onClick={()=>handleChangeLanguages('vi')}>
+                <button onClick={() => handleChangeLanguages('vi')}>
                     Vietnam
                 </button>
             </nav>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <p>{t('my translated text')}</p>
             </header>
         </div>
     );
