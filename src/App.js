b54a43b3ac5165ffd9523a2552f39f050a10a5ad
DEVfancybear <duonghacker2345@gmail.com>
2020-05-01 21:45:57 +0700

    onclick change languages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useTranslation} from 'react-i18next';
 
 const App = () => {
+    const {t, i18n} = useTranslation();
+    const handleChangeLanguages = (language) => {
+        i18n.changeLanguage(language);
+
+    }
     return (
         <div className="App">
             <nav style={{width: '100%', padding: '2rem 0', backgroundColor: 'gray'}}>
-                <button>
+                <button onClick={()=>handleChangeLanguages('en')}>
                     English
                 </button>
-                <button>
+                <button onClick={()=>handleChangeLanguages('ko')}>
                     Korean
                 </button>
-                <button>
+                <button onClick={()=>handleChangeLanguages('chi')}>
                     Chinese
                 </button>
-                <button>
+                <button onClick={()=>handleChangeLanguages('vi')}>
                     Vietnam
                 </button>
             </nav>
